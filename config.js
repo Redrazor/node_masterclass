5021646e8d82c6ee0737ffd8c716e34010221fee
Redrazor <red.razor.design@gmail.com>
2018-08-20 10:43:55 +0100

    Update repo with recent changes

--- config.js
+++ config.js
@@ -2,15 +2,66 @@
 let environments = {};
 
 environments.development = {
+  'phonelength': 12,
+  'randomstringlength': 20,
   'httpPort': 1337,
   'httpsPort':1338,
-  'envName': 'development'
+  'envName': 'development',
+  'hashingSecret': 'thisisthedevsecret',
+  'maxChecks': 5,
+  'twilio' : {
+    'accountSid' : 'ACb32d411ad7fe886aac54c665d25e5c5d',
+    'authToken' : '9455e3eb3109edc12e3d8c92768f7a67',
+    'fromPhone' : '+15005550006'
+  },
+    'templateGlobals': {
+        'appName': 'UptimeChecker',
+        'companyName': 'Redrazor Inc',
+        'yearCreated': '2018',
+        'baseUrl': 'http://localhost:1337'
+    }
+};
+
+environments.staging = {
+  'phonelength': 12,
+  'randomstringlength': 20,
+  'httpPort': 1337,
+  'httpsPort':1338,
+  'envName': 'development',
+  'hashingSecret': 'thisisthedevsecret',
+  'maxChecks': 5,
+  'twilio' : {
+    'accountSid' : 'ACb32d411ad7fe886aac54c665d25e5c5d',
+    'authToken' : '9455e3eb3109edc12e3d8c92768f7a67',
+    'fromPhone' : '+15005550006'
+  },
+    'templateGlobals': {
+        'appName': 'UptimeChecker',
+        'companyName': 'Redrazor Inc',
+        'yearCreated': '2018',
+        'baseUrl': 'http://Sky-Labs-redrazordesign470983.codeanyapp.com:1337'
+    }
 };
 
 environments.production = {
+  'phonelength': 12,
+  'randomstringlength': 20,
   'httpPort': 3000,
   'httpsPort':3001,
-  'envName': 'production'
+  'envName': 'production',
+  'hashingSecret': 'thisistheprodsecret',
+  'maxChecks': 5,
+  'twilio' : {
+    'accountSid' : 'ACb32d411ad7fe886aac54c665d25e5c5d',
+    'authToken' : '9455e3eb3109edc12e3d8c92768f7a67',
+    'fromPhone' : '+15005550006'
+  },
+    'templateGlobals': {
+        'appName': 'UptimeChecker',
+        'CompanyName': 'Redrazor Inc',
+        'YearCreated': '2018',
+        'baseUrl': 'http://Sky-Labs-redrazordesign470983.codeanyapp.com:3000'
+    }
 };
 
 let currentEnvironment = typeof(process.env.NODE_ENV) == 'string' ? process.env.NODE_ENV.toLowerCase() : '';
--- index.js
+++ index.js
@@ -1,106 +1,23 @@
+/** Primary Entry Point**/
 
-const http = require('http');
-const https = require('https');
-const url = require('url');
-const StringDecoder = require('string_decoder').StringDecoder;
-const config = require('./config');
-const fs  = require('fs');
+const server = require('./lib/server');
 
-//Instantiate HTTP Server
-const httpServer = http.createServer(function(req, res) {
-    unifiedServer(req,res);
-});
+const workers = require('./lib/workers');
 
-//Listen on HTTP Server
-httpServer.listen(config.httpPort, function() {
-    console.log(`Listening on port ${config.httpPort}`);
-});
+//Declare app
 
-let httpsServerOptions = {
-    'key': fs.readFileSync('./https/key.pem'),
-    'cert': fs.readFileSync('./https/cert.pem'),
+let app = {};
+
+//Init 
+app.init = function(){
+  //Start the server
+  server.init();
+  //Start the workers
+  workers.init();
 };
 
-//Instantiate HTTPS Server
-const httpsServer = https.createServer(httpsServerOptions,function(req, res) {
-    unifiedServer(req,res);
-});
+//Start Init
 
-//Listen on HTTPS Server
-httpsServer.listen(config.httpsPort, function() {
-    console.log(`Listening on port ${config.httpsPort}`);
-});
+app.init();
 
-//Internal server logic handling
-let unifiedServer = function(req,res){
-    let parsedUrl = url.parse(req.url, true);
-
-    let path = parsedUrl.pathname;
-    let trimmedPath = path.replace(/^\/+|\/+$/g, '');
-
-    let method = req.method.toLowerCase();
-
-    let queryString = parsedUrl.query;
-
-    let headers = req.headers;
-    
-    //Payload parsing
-    let decoder = new StringDecoder('utf-8');
-    let buffer = '';
-    
-    req.on('data', function(data){
-        buffer += decoder.write(data);
-    });
-    
-    req.on('end', function(){
-        buffer  += decoder.end();
-        
-        let chosenHandler = typeof(router[trimmedPath]) !== 'undefined' ? router[trimmedPath] : handlers.notFound;
-        
-        let data = {
-            'trimmedPath': trimmedPath,
-            'queryString': queryString,
-            'method': method,
-            'headers': headers,
-            'payload': buffer
-        };
-        
-        //route the request
-        chosenHandler(data, function(statusCode, payload){
-            //Default status code
-            statusCode = typeof(statusCode) == 'number' ? statusCode : 200;
-            
-            payload = typeof(payload) == 'object' ? payload : {};
-            
-            let payloadString = JSON.stringify(payload);
-            
-            res.setHeader('Content-Type','application/json');
-            res.writeHead(statusCode);
-            res.end(payloadString);
-        });
-       
-        console.log(`${method} Request received on path: ${trimmedPath} and query string`, queryString);
-        console.log(`Request received with headers: `, headers);
-        console.log(`Request received with payload: `, buffer);
-    });
-};
-
-//Handlers
-let handlers = {};
-
-handlers.ping = function(data,callback){
-    callback(200);
-};
-
-handlers.hello = function(data, callback){
-    callback(200, { 'message': 'Hello to you too, friend.'});
-};
-
-handlers.notFound = function(data, callback){
-    callback(404);
-};
-//Router
-let router = {
-    'ping': handlers.ping,
-    'hello': handlers.hello
-}
+module.exports = app;
